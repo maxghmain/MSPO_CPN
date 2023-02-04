031e5223fe041704b76f64be59bf56b11c320a21
AntNamphol <89235281+AntNamphol@users.noreply.github.com>
2023-02-04 14:06:56 +0700

    Update

--- component/content/afb_menu/function/selectShowOderAFBstate.php
+++ component/content/afb_menu/function/selectShowOderAFBstate.php
@@ -1,121 +1,111 @@
 <style>
-    .box-afb {
-        margin: 10px;
-        display: block;
-        border: 1px solid #006ebc;
-        width: 425px;
-        border-radius: 10px;
-    }
 
-    .text-box {
-        /*border: 1px solid red;*/
-        margin: 5px;
-    }
-
-    #bikho {
-        text-align: center;
-        background: #006ebc;
-        color: white;
-        font-size: 20px;
-        font-weight: bold;
-        border-radius: 10px;
-        
-    }
-
-    #num_afb {
-        /* border: 1px solid red;*/
-        display: flex;
-    }
-
-    #num_afb-1 {
+    #body-afb-box-1{
         width: 100%;
+        border: 1px solid black;
+        margin: 10px;
+        border-radius: 10px;
+        box-shadow: rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;
     }
-    #kuy{
-        border: 1px solid red;
-        width: 425px;
+    #body-afb-box-2{
+ 
+        margin: 2.5px;
     }
+    #title-afb{
+        justify-content: center;
+        align-items: center ;
+        height: 50px;
+        background: #006ebc;
+        display: flex;
+        text-align: center;
+        font-size: 22px;
+        font-weight: bold;
+        color: white;
+        border-radius: 10px;
+        box-shadow: rgba(0, 0, 0, 0.07) 0px 1px 1px, rgba(0, 0, 0, 0.07) 0px 2px 2px, rgba(0, 0, 0, 0.07) 0px 4px 4px, rgba(0, 0, 0, 0.07) 0px 8px 8px, rgba(0, 0, 0, 0.07) 0px 16px 16px;
+    }
+   #info-afb-box-1{
+    display: flex;
+   }
+   #info-afb-box-2{
+    width: 100%;
+   }
 </style>
-<div class="box-afb">
-    <div class="text-box">
-        <div class="ohio">
-        <?php
-        include 'php/connect_db.php';
-        $form_afb_id = $_SESSION['form_afb_id'];
-        $sql = "SELECT a.order_afb_id, b.name_ms_normal_name, b.name_ms_real_name, a.order_afb_value, c.unit_name, a.order_afb_note 
-FROM order_afb_tbl as a 
-INNER JOIN name_ms_tbl as b ON a.name_ms_id = b.name_ms_id 
-INNER JOIN unit_tbl as c ON a.unit_id = c.unit_id 
-WHERE a.form_afb_id = '$form_afb_id'";
-        $query = mysqli_query($conn, $sql);
-        $count_data = 1;
 
-        $sql = "SELECT * FROM form_afb_tbl";
-        $result = $conn->query($sql);
-        $count_data = 1;
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                echo '<div id="bikho">';
+            <?php
+            include 'php/connect_db.php';
+            $sql = "SELECT * FROM form_afb_tbl WHERE state_id = 1";
+            $result = mysqli_query($conn, $sql);
+            while ($row = mysqli_fetch_array($result)) {
+                echo '<div id="body-afb-box-1">';
+                echo '<div id="body-afb-box-2">';
+                echo '<div id="title-afb">';
                 echo 'ใบขอซื้อ';
                 echo '</div>';
-                echo '<div id="num_afb">';
-                echo '<div id="num_afb-1">';
+                echo '<br>';
+                echo '<div id="info-afb-box-1">';
+                echo '<div id="info-afb-box-2">';
                 echo 'เล่มที่ :' . $row['form_afb_number'];
                 echo '</div>';
-                echo '<div id="num_afb-1">';
+                echo '<div id="info-afb-box-2">';
                 echo 'เลขที่ :' . $row['form_afb_book_number'];
                 echo '</div>';
                 echo '</div>';
-                echo '<div id="num_afb">';
-                echo '<div id="num_afb-1">';
+
+                echo '<div id="info-afb-box-1">';
+                echo '<div id="info-afb-box-2">';
                 echo 'วันที่เขียน :' . $row['form_afb_write_date'];
                 echo '</div>';
-                echo '<div id="num_afb-1">';
+                echo '<div id="info-afb-box-2">';
                 echo 'วันที่เพิ่มลงระบบ :' . $row['form_afb_savesys_date'];
                 echo '</div>';
                 echo '</div>';
-                echo '<div id="num_afb">';
-                echo '<div id="num_afb-1">';
+
+                echo '<div id="info-afb-box-1">';
+                echo '<div id="info-afb-box-2">';
                 echo 'ผู้ขอซื้อ :' . $row['form_afb_people_name'];
                 echo '</div>';
-                echo '<div id="num_afb-1">';
+                echo '<div id="info-afb-box-2">';
                 echo 'ผู้อนุมัติ :' . $row['form_afb_people_name_ok'];
+               
                 echo '</div>';
                 echo '</div>';
-                echo '<div id="num_afb-1">';
                 echo 'ใช้งานกับ :' . $row['form_afb_detail_work_for'];
+   
+                $form_afb_id = $row['form_afb_id'];
+                $sql = "SELECT order_afb_id,name_ms_normal_name,name_ms_real_name,order_afb_value,unit_name,order_afb_note FROM order_afb_tbl as a INNER JOIN name_ms_tbl as b ON a.name_ms_id = b.name_ms_id INNER JOIN unit_tbl as c ON a.unit_id = c.unit_id WHERE form_afb_id = $form_afb_id ";
+                $count_data = 1;
+                $result1 = mysqli_query($conn, $sql);
+                while ($row1 = mysqli_fetch_array($result1)) {
+                    echo '<table >';
+                    echo '<thead>';
+                    echo '<tr>';
+                    echo '<td style="width:6%">';
+                    echo $count_data;
+                    echo '</td>';
+                    echo '<td style="width:20%">';
+                    echo $row1['name_ms_normal_name'];
+                    echo '</td>';
+                    echo '<td style="width:20%"> ';
+                    echo $row1['name_ms_real_name'];
+                    echo '</td>';
+                    echo '<td style="width:20%">';
+                    echo $row1['order_afb_value'];
+                    echo $row1['unit_name'];
+                    echo '</td>';
+                    echo '<td style="width:20%">';
+                    echo $row1['order_afb_note'];
+                    echo '</td>';
+                    echo '<tr>';
+                    echo '</table>';
+
+
+                    $count_data ++;
+                }
+                echo '<br/>';
                 echo '</div>';
-                
+                echo '</div>';
             }
-            while ($row = mysqli_fetch_array($query)) {
-                echo '<table >';
-                echo '<thead>';
-                echo '<tr>';
-                echo '<td style="width:6%">';
-                echo $count_data;
-                echo '</td>';
-                echo '<td style="width:20%">';
-                echo $row['name_ms_normal_name'];
-                echo '</td>';
-                echo '<td style="width:20%"> ';
-                echo $row['name_ms_real_name'];
-                echo '</td>';
-                echo '<td style="width:20%">';
-                echo $row['order_afb_value'];
-                echo $row['unit_name'];
-                echo '</td>';
-                echo '<td style="width:20%">';
-                echo $row['order_afb_note'];
-                echo '</td>';
-                echo '<tr>';
-                echo '</table>';
-
-
-                $count_data++;
-            }
-        }
-        // Close the database connection
-        $conn->close();
-        ?>
-        </div>
-    </div>
-</div>
+            // Close the database connection
+            $conn->close();
+            ?>
--- component/content/afb_menu/state_afb.php
+++ component/content/afb_menu/state_afb.php
@@ -5,14 +5,21 @@
     #set-con {
         margin: 20px;
         /*width: 100%;*/
-        border: 2px solid #b7b7b7;
+       /* border: 2px solid #b7b7b7;*/
         height: auto;
         border-radius: 20px;
+        /*display: flex;*/
+    }
+    .margin-box{
+    
+       /* border: 1px solid red;*/
         display: flex;
     }
+    
 </style>
 
 <div id="set-con">
+    <div class="margin-box">
        <?php include 'function/selectShowOderAFBstate.php';?>
-       
+       </div>
 </div>
