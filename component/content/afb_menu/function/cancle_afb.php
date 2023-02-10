1c709c4258fe1e162027520e8bf664b7812120a3
AntNamphol <89235281+AntNamphol@users.noreply.github.com>
2023-02-10 13:16:13 +0700

    update

--- component/content/afb_menu/function/cancle_afb.php
+++ component/content/afb_menu/function/cancle_afb.php
@@ -2,8 +2,8 @@
 session_start();
 include 'php/connect_db.php';
 $sql = "UPDATE form_afb_tbl SET state_id = 6 WHERE form_afb_id = $form_afb_id ";
-$q=mysqli_query($conn,$sql);
-if (!$q) {
-    die('Error: ' . mysqli_error($conn));
-}
+mysqli_query($conn,$sql);
+$sql = "UPDATE order_afb_tbl SET state_id = 7 WHERE form_afb_id = $form_afb_id ";
+mysqli_query($conn,$sql);
+
 ?>
